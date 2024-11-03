d515d2a5b0e41dcdadd63775c6028d3c38acd1cd
andresgiir2 <159092084+andresgiir2@users.noreply.github.com>
2024-11-02 22:32:45 -0500

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductsComponent } from './features/products/products.component';
-import { CartComponent } from './features/cart/cart.component';
-
-export const routes: Routes = [
-    {path: 'products', component: ProductsComponent},
-    {path: 'cart', component: CartComponent},
-    {path: '', redirectTo: '/products', pathMatch: 'full'},
-];
